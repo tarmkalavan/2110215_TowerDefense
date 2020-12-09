4ff877abdf7057bcc9d74db3e7e400c6c72b350c
Tarm <tk.kalavan@gmail.com>
2020-12-09 15:37:56 +0700

    added basic methods for ammo

--- src/base/Ammo.java
+++ src/base/Ammo.java
@@ -1,5 +1,27 @@
 package base;
 
 public class Ammo {	
+	protected int damage;
+	protected String damageType; //??
+	protected String debuff; //??
+	protected int splashRadius;
 	
+	public Ammo(int damage, int splashRadius) {
+		setDamage(damage);
+		setSplashRadius(splashRadius);
+	}
+	
+	public void setDamage(int damage) {
+		this.damage = Math.max(0, damage);
+	}
+	
+	public void setSplashRadius(int splashRadius) {
+		this.splashRadius = Math.max(0, splashRadius);
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public int getSplashRadius() {
+		return splashRadius;
+	}
 }
