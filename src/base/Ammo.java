3a7dce2587c4c4d3f2dcd9717cde9c54c4487d00
Tarm <tk.kalavan@gmail.com>
2020-12-09 15:48:31 +0700

    copy-pasted Tower, Monster, Ammo, Coordinate, Tilemap again to fix issue with LF CRLF

--- src/base/Ammo.java
+++ src/base/Ammo.java
@@ -1,26 +1,28 @@
 package base;
 
-public class Ammo {	
+public class Ammo {
 	protected int damage;
-	protected String damageType; //??
-	protected String debuff; //??
+	protected String damageType; // ??
+	protected String debuff; // ??
 	protected int splashRadius;
-	
+
 	public Ammo(int damage, int splashRadius) {
 		setDamage(damage);
 		setSplashRadius(splashRadius);
 	}
-	
+
 	public void setDamage(int damage) {
 		this.damage = Math.max(0, damage);
 	}
-	
+
 	public void setSplashRadius(int splashRadius) {
 		this.splashRadius = Math.max(0, splashRadius);
 	}
+
 	public int getDamage() {
 		return damage;
 	}
+
 	public int getSplashRadius() {
 		return splashRadius;
 	}
--- src/base/Tower.java
+++ src/base/Tower.java
@@ -3,14 +3,95 @@ package base;
 public abstract class Tower {
 	
 	protected int attackDamage;
-	protected int attackSpeed;
+	protected double attackSpeed;
 	protected int range;
 	protected int upgradeCost;
 	protected int sellCost;
 	protected int buyCost;
+	protected int level;
+	protected double upgradeBonus;
+	protected Ammo ammo;
 	
 	public abstract void upgradeTower();
+
+	public Tower(int attackDamage, double attackSpeed, int range,
+			   int upgradeCost, int sellCost, int level, double upgradeBonus, 
+			   Ammo ammo, int splashRadius) {
+		setAttackSpeed(attackSpeed);
+		setRange(range);
+		setUpgradeCost(upgradeCost);
+		setSellCost(sellCost);
+		setLevel(level);
+		setUpgradeBonus(upgradeBonus);
+		ammo = new Ammo(attackDamage, splashRadius);
+	}
 	
-	
+	public int getAttackDamage() {
+		return attackDamage;
+	}
+
+	public void setAttackDamage(int attackDamage) {
+		this.attackDamage = Math.max(attackDamage, 0);
+	}
+
+	public double getAttackSpeed() {
+		return attackSpeed;
+	}
+
+	public void setAttackSpeed(double attackSpeed) {
+		this.attackSpeed = Math.max(attackSpeed,0.0);
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = Math.max(range, 0);
+	}
+
+	public int getUpgradeCost() {
+		return upgradeCost;
+	}
+
+	public void setUpgradeCost(int upgradeCost) {
+		this.upgradeCost = Math.max(upgradeCost, 0);
+	}
+
+	public int getSellCost() {
+		return sellCost;
+	}
+
+	public void setSellCost(int sellCost) {
+		this.sellCost = Math.max(sellCost, 0);
+	}
+
+	public int getBuyCost() {
+		return buyCost;
+	}
+
+	public void setBuyCost(int buyCost) {
+		this.buyCost = Math.max(sellCost, 0);
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = Math.max(level, 0);
+	}
+
+	public double getUpgradeBonus() {
+		return upgradeBonus;
+	}
+
+	public void setUpgradeBonus(double upgradeBonus) {
+		this.upgradeBonus = Math.max(upgradeBonus, 0.0);
+	}
+
+	public Ammo getAmmo() {
+		return ammo;
+	}
 
 }
