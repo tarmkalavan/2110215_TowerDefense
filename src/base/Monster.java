858af3e9a33865d5d082a89a6c3edf502e877dc4
Tarm <tk.kalavan@gmail.com>
2020-12-19 22:16:06 +0700

    fix monster

--- src/base/Monster.java
+++ src/base/Monster.java
@@ -47,7 +47,7 @@ public abstract class Monster implements Effectable{
                 moveX = false;
                 nodeDirection++;
                 // Traversed all changing points, path ended
-                if(nodeDirection == path.size()){
+                if(nodeDirection == path.size() - 1){
                     pathFinished = true;
                     isDead = true;
                 }
